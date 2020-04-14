8a8692f281d285e1de885b318351b8de3672b1a4
ju1c31st <jonah.lee.skinner@gmail.com>
2020-04-14 14:26:29 +1000

    typo fixes

--- Problem12372.java
+++ Problem12372.java
@@ -9,13 +9,11 @@ class Main {
 		int case = 0;
 		int testCases;
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-
 		t = br.readLine());
 		if(t <= 100 && t != null){
 			testCases = t;S
 			for(int i=0; i<testCases; i++){
 				if(t = br.readLine() != null){
-					System.out.println();
 					System.out.println("case " + case + ": good");
 					System.out.println("case " + case + ": bad");
 				}
