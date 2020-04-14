4c874f49a2da9b632280e7ecee7a62666e707110
Jonah Skinner <jonah.lee.skinner@gmail.com>
2020-04-14 14:15:41 +1000

    Create Problem12372.java

---
+++ Problem12372.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+	public static void main (String[]args) throws IOException  {
+		int case = 0;
+		int testCases;
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+		t = br.readLine());
+		if(t <= 100 && t != null){
+			testCases = t;
+			for(int i=0; i<testCases; i++){
+				if(t = br.readLine() != null){
+					System.out.println()
+					System.out.println("case " + case + ": good");
+					System.out.println("case " + case + ": bad");
+				}
+			}
+		}
+		return 1;
+	}
+}
