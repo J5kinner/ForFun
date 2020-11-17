cb17973f24d7fe314efae67c47b4a9c326d8bac9
Mungo_ <bailey.gibbons2018@gmail.com>
2020-11-17 17:35:03 +1100

    This should work now

--- secret-santa.py
+++ secret-santa.py
@@ -24,19 +24,23 @@ def usage():
     exit(0)
 
 
+class MyUser:
+    def __init__(self, user_object, enabled):
+        self.user = user_object
+        self.enabled = enabled
+
+
 if len(sys.argv) != 1 and sys.argv[1] == "-h":
     usage()
 
 testMode = "-t" in sys.argv
 selectMode = "-s" in sys.argv
 
-
 # login to facebook here
 while True:
     try:
-        username = input("Host email > ")
-        password = input("Host password > ")
-
+        username = input("Host username > ").rstrip()
+        password = input("Host password > ").rstrip()
         client = Client(username, password, None, 1)
     except FBchatException:
         print("Login failed...")
@@ -50,53 +54,89 @@ while True:
 # get list of user IDs from group thread
 while True:
     try:
-        groupID = input("Group ID > ")
+        groupID = input("Group ID > ").rstrip()
+        print("group: {}".format(groupID))
         inputGroup = client.fetchGroupInfo(groupID).get(groupID)
+
     except FBchatException:
         print("Could not find a group with this ID...")
-    #except:
-     #   print("There has been an error collecting the group info...")
+    # except:
+    #   print("There has been an error collecting the group info...")
     else:
         print("\nGroup \"{}\" found!".format(inputGroup.name))
         if selectMode:
             confirmation = input("Select from users in this group? [Y/n] > ")
         else:
-            confirmation = input("Send messages to all users in this group? [Y/n] > ")
+            confirmation = input("Send messages to all other users in this group? [Y/n] > ")
         if confirmation.lower()[0] == 'y':
             break
 
 inputParticipantDict = client.fetchUserInfo(*list(inputGroup.participants))
+inputParticipants = inputParticipantDict.values()
+myUserList = []
 
-if client.uid in inputParticipantDict.keys():
-    del inputParticipantDict[client.uid]
 # sort the list randomly and store the head of the list
 # each participant is assigned the next member of the list, and the last participant is assigned the first.
-print("Host excluded: {}".format(client.name))
-print("Count: {}, Inputs: ".format(len(inputParticipantDict)))
-for value in inputParticipantDict.values():
-    print(value.name)
+print("Host excluded: {}".format(client.fetchUserInfo(client.uid).get(client.uid).name))
+print("\nTotal people in group: {} ".format(len(inputParticipants)))
+count = 0
+for user in inputParticipants:
+    myUserList.append(MyUser(user, user.uid != client.uid))
 
+
+# Allow the user to toggle participants
+while True:
+    for i in range(len(myUserList)):
+        if myUserList[i].enabled:
+            symbol = '+'
+        else:
+            symbol = ' '
+        print("{}. [{}] {}".format(i, symbol, myUserList[i].user.name))
+
+    action = input("\nSelect a number to toggle, press \'Y\' to continue or \'N\' to cancel > ")
+    print("\n")
+    if action[0].lower() == 'y':
+        break
+    elif action[0].lower() == 'n':
+        print("Cancelling... No messages have been sent.")
+        exit(0)
+    else:
+        try:
+            action = int(action)
+            if 0 <= action < len(myUserList):
+                myUserList[action].enabled = not myUserList[action].enabled
+        except ValueError:
+            print("That is not a valid input.")
+        finally:
+            print("")
+
+
+# make a list in a random order, only adding enabled users
 sortedParticipants = []
-while len(inputParticipantDict) > 0:
-    i = random.randrange(len(inputParticipantDict))
-    sortedParticipants.append(inputParticipantDict.pop(i))
+while len(myUserList) > 0:
+    i = random.randrange(len(myUserList))
+    if myUserList[i].enabled:
+        sortedParticipants.append(myUserList.pop(i))
+    else:
+        myUserList.pop(i)
+
 
 # send a message to all participants with the name of the next person in the list
 if len(sortedParticipants) != 0:
     for i in range(len(sortedParticipants)):
-        if i == len(sortedParticipants)-1:
+        if i == len(sortedParticipants) - 1:
             j = 0
         else:
-            j = i+1
-        thisUID = sortedParticipants[i]
-        nextUID = sortedParticipants[j]
+            j = i + 1
+        thisUser = sortedParticipants[i].user
+        nextUser = sortedParticipants[j].user
         try:
-
             if testMode:
-                print("{} --> {}".format(thisUID.name, nextUID.name))
+                print("{} --> {}".format(thisUser.name, nextUser.name))
             else:
-                # client.send(Message(text="Hi there! Your Secret Santa is {}!".format(nextUID.name), thread_id=thisUID,thread_type=ThreadType.USER))
+                client.send(Message(text="Hi there! Your Secret Santa is {}!".format(nextUser.name)), thread_id=thisUser.uid,thread_type=ThreadType.USER)
                 print("sent text to person {}".format(i))
-        except:
-            print("Error sending message to {}".format(thisUID.name))
-
+        except FBchatException:
+            print("Error sending message to {}".format(thisUser.name))
+if not testMode:
+    print("Messages have been sent.")
