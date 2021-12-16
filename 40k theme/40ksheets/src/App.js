a343f9337e8deea802f4170b69236d3c0fc4258e
Jonah Skinner <jonah.lee.skinner@gmail.com>
2021-12-16 16:26:37 +1100

    added footer and instructions

--- 40k theme/40ksheets/src/App.js
+++ 40k theme/40ksheets/src/App.js
@@ -9,9 +9,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div className="main-section">
+          <p>Instructions:</p>
+          <p>1. Download HTML file from battlescribe</p>
+          <p>2. Upload your HTML file to which style you prefer</p>
           <FileUploader />
         </div>
       </header>
+      <footer><p>Proudly Made by <a href="https://jonahskinner.com/">Jonah Skinner</a></p></footer>
     </div>
   );
 }
