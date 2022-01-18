bfd8a03bd17923b6a4eb9495b4b81b2b4cf718a3
Jonah Skinner <jonah.lee.skinner@gmail.com>
2022-01-18 12:46:37 +1100

    Left shifting arrays

---
+++ onlineJudge/HackerRank/Left-Rotation.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'rotLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER d
+ */
+
+function rotLeft(a, d) {
+    // Write your code here
+    // loop until rotations are done
+    // shallow copies of array with rotations
+    // A[1] === A[last], 
+    // return final array
+    let arrRotated = a;
+    while(d--) {
+        arrRotated.push(arrRotated.shift());
+        
+    }
+    return arrRotated;
+    
+    
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const d = parseInt(firstMultipleInput[1], 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = rotLeft(a, d);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
