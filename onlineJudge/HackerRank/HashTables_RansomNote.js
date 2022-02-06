3c24e88f32afc535e42184305104c37e8e9356f0
Jonah Skinner <jonah.lee.skinner@gmail.com>
2022-02-06 16:29:48 +1100

    Finished hashTables ransomNote

--- onlineJudge/HackerRank/HashTables_RansomNote.js
+++ onlineJudge/HackerRank/HashTables_RansomNote.js
@@ -38,61 +38,23 @@ function checkMagazine(magazine, note) {
   //if theres a new word in note (case sensitive words included) -> NO
   //if every word in note exists in mag -> YES
 
-  const ht = new HashTable();
-  for (const word of magazine) {
-    ht.add(word, word);
-  }
-  for (const term of note) {
-    ht.search(term);
-  }
-  if (bool === true) {
-    console.log(ht);
-
-    return console.log("Yes");
-  } else {
-    console.log(ht);
-
-    return console.log("No");
-  }
-}
-
-class HashTable {
-  //grab our magazine and turn it into a hash map
-  constructor() {
-    this.values = {};
-    this.length = 0;
-    this.size = 0;
-  }
-  calculateHash(key) {
-    return key.toString().length % this.size;
-  }
-
-  add(key, value) {
-    const hash = this.calculateHash(key);
-    if (!this.values.hasOwnProperty(hash)) {
-      this.values[hash] = {};
+    let map = { };
+    let replicable = true;
+    for (let i of magazine) {
+        map[i] = (map[i] || 0) + 1;
     }
-    if (!this.values[hash].hasOwnProperty(key)) {
-      this.length++;
+    for (let i of note) {
+        map[i] = (map[i] || 0) - 1;
     }
-    this.values[hash][key] = value;
-  }
-
-  search(key) {
-    const hash = this.calculateHash(key);
-    if (
-      this.values.hasOwnProperty(hash) &&
-      this.values[hash].hasOwnProperty(key) &&
-      this.values[hash][key] !== undefined
-    ) {
-      // console.log(this.values[hash][key]);
-      //eliminate duplicates
-      this.values[hash][key] = undefined;
-    } else {
-      bool = false;
-      console.log("Changed here at: " + this.values[hash][key]);
+    for (let i in map) {
+        if(!replicable) {
+            break;
+        } else if(map[i] < 0) {
+            replicable = false;
+        }
     }
-  }
+  //   console.log(map);
+    console.log(replicable ? "Yes" : "No");
 }
 
 function main() {
