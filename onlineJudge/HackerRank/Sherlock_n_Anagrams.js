a9c6f3eb7b9803db23fcb1664001c78417c9b4e3
Jonah Skinner <jonah.lee.skinner@gmail.com>
2022-02-08 16:38:14 +1100

    Cleaning up the code

--- onlineJudge/HackerRank/Sherlock_n_Anagrams.js
+++ onlineJudge/HackerRank/Sherlock_n_Anagrams.js
@@ -1,25 +1,25 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', function (inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', function () {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 /*
@@ -30,78 +30,37 @@ function readLine() {
  */
 
 function sherlockAndAnagrams(s) {
-    // Write your code here
-    //AnagramCount;
-    //map of our anagrams;
-    //Construct hash table of potential anagrams
-let result = []
-let count = 0
+  // Write your code here
+  let result = [];
+  let count = 0;
   for (let i = 0; i < s.length; i++) {
-      for (let j = i + 1; j < s.length + 1; j++) {
-          result.push(s.slice(i, j).split("").sort().join(""));
-      }
+    for (let j = i + 1; j < s.length + 1; j++) {
+      result.push(s.slice(i, j).split("").sort().join(""));
+    }
   }
-  for(let i=0;i<result.length-1;i++){
-      for(let j=i+1;j<result.length;j++){
-          if((result[i]==result[j]) && result[i].length === result[j].length ){
-              count++;
-                console.log("Comparing " + result[i] + " and "+ result[j]);
-
-          } 
+  for (let i = 0; i < result.length - 1; i++) {
+    for (let j = i + 1; j < result.length; j++) {
+      if (result[i] == result[j] && result[i].length === result[j].length) {
+        count++;
+        console.log("Comparing " + result[i] + " and " + result[j]);
       }
+    }
   }
-  return count
-// }
-//     //Find every possibility for the string selected and store it in map
-//     let subsets = [];
-//     const anagrams = new Map();
-//     let count = 0;
-//     for (let key of s) {
-//         // console.log(key);
-//          anagrams.set(key);
-//     }
-//     // console.log([...anagrams.entries()]);
-
-//     for (let i = 0; i < s.length; i++) { //loop through whole string
-//         for (let j = i + 1; j < s.length +1; j++) { //loop up to the current string we are on        
-//             let subset = s.slice(i, j).split("").sort().join("");
-//             // console.log("Before Checking: " + anagrams.get(subset));
-//             if (anagrams.get(subset)) {
-//                 console.log("subset found: " + subset + " and anagram has " + anagrams.get(subset) + " ++! ");
-
-//                 count++;
-//             }
-            
-//             anagrams.set(subset, subset);
-//             // console.log(anagrams.get(subset));
-
-
-//         }
-
-//         console.log([...anagrams.keys()]);
-
-//         console.log("Count is now " + count);
-//     //    for(let x of anagrams) {
-//     //        console.log(x);
-//     //    }
-//     }
-
-
+  return count;
 }
 
-
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const q = parseInt(readLine().trim(), 10);
+  const q = parseInt(readLine().trim(), 10);
 
-    for (let qItr = 0; qItr < q; qItr++) {
-        const s = readLine();
+  for (let qItr = 0; qItr < q; qItr++) {
+    const s = readLine();
 
-        const result = sherlockAndAnagrams(s);
+    const result = sherlockAndAnagrams(s);
 
-        ws.write(result + '\n');
-    }
+    ws.write(result + "\n");
+  }
 
-    ws.end();
+  ws.end();
 }
