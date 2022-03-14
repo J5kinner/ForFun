9ce8a52979dc32795a303e2a8015b5b72184ffc3
Jonah Skinner <jonah.lee.skinner@gmail.com>
2022-03-14 18:38:44 +1100

    Turns out to pass all tests! on kattis

--- onlineJudge/Kattis/baby_bites/Baby.js
+++ onlineJudge/Kattis/baby_bites/Baby.js
@@ -1,42 +1,41 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 let first = true;
 let allMumble = true;
 let fishy = false;
-rl.on('line', (line) => {
-    
-
-    if(first) {
-        n = +line;
-        first = false;
-        return;
+rl.on("line", (line) => {
+  if (first) {
+    n = +line;
+    first = false;
+    return;
+  }
+  const array = line.split(" ");
+  for (let i = 0; i < n; i++) {
+    if (
+      parseInt(array[i]) !== i + 1 &&
+      array[i] !== "mumble" &&
+      parseInt(array[i]) !== undefined &&
+      array[i] !== undefined &&
+      !first
+    ) {
+      fishy = true;
     }
-    const array = line.split(' ');
-    for(let i = 0; i<n; i++) {
-
-        if((parseInt(array[i]) !== i+1) && (array[i] !== "mumble") && (parseInt(array[i]) !== undefined) && (array[i] !== undefined) && !first){
-            fishy = true;
-            console.log( array[i] +" trig " + (i+1));
-        }
-        if(array[i] != "mumble") {
-            allMumble = false;
-        } 
-        // console.log(array[i]);
+    if (array[i] != "mumble") {
+      allMumble = false;
     }
-    if(allMumble) {
-        console.log("makes sense");
-        return;
-    }
-    if(fishy) {
-        console.log("something is fishy");
-    } else {
-        console.log("makes sense");
-    }
+  }
+  if (allMumble) {
+    console.log("makes sense");
+    return;
+  }
+  if (fishy) {
+    console.log("something is fishy");
+  } else {
+    console.log("makes sense");
+  }
 });
-
-
