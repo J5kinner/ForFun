94b0b992a58e8a8990a0227829215fe07adbf51f
Jonah Skinner <jonah.lee.skinner@gmail.com>
2022-03-14 18:32:44 +1100

    problem with previous file iteration data persisting over to the next interation altering results

--- onlineJudge/Kattis/baby_bites/Baby.js
+++ onlineJudge/Kattis/baby_bites/Baby.js
@@ -4,25 +4,24 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
 let first = true;
 let allMumble = true;
 let fishy = false;
 rl.on('line', (line) => {
+    
+
     if(first) {
         n = +line;
         first = false;
+        return;
     }
-    var array = line.split(' ');
+    const array = line.split(' ');
     for(let i = 0; i<n; i++) {
-        if(array[i] === "mumble"){
-            if(!allMumble && !array[0] && !array[array.length]){
-                if((array[i-1] - array[i+1]) > 2){
-                    fishy = true;
-                }
 
-            }
-            
-            console.log("mumble detected");
+        if((parseInt(array[i]) !== i+1) && (array[i] !== "mumble") && (parseInt(array[i]) !== undefined) && (array[i] !== undefined) && !first){
+            fishy = true;
+            console.log( array[i] +" trig " + (i+1));
         }
         if(array[i] != "mumble") {
             allMumble = false;
@@ -30,10 +29,14 @@ rl.on('line', (line) => {
         // console.log(array[i]);
     }
     if(allMumble) {
-        console.log("makes sense")
-    } else if(fishy) {
+        console.log("makes sense");
+        return;
+    }
+    if(fishy) {
         console.log("something is fishy");
     } else {
         console.log("makes sense");
     }
 });
+
+
