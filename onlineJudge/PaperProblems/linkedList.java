43577cb7354aa1fbedf1ad3359628dce58233399
Jonah Skinner <jonah.lee.skinner@gmail.com>
2022-03-25 19:23:38 +1100

    Weekly Problemsets

---
+++ onlineJudge/PaperProblems/linkedList.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class linkedList {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        System.out.println("Hello world");
+    }
+}
