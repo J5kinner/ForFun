4071616b976da3c0732d6ee26bdfc627dce51f73
Jonah Skinner <jonah.lee.skinner@gmail.com>
2022-04-03 17:14:28 +1000

    Grinding Kattis

--- 40ksheets/src/assets/images copy/sharp.js
+++
@@ -1,9 +0,0 @@
-const sharp = require('sharp');
-const fs = require('fs');
-const directory = './';
-
-fs.readdirSync(directory).forEach(file => {
-  sharp(`${directory}/${file}`)
-    .resize(466, 606) // width, height
-    .toFile(`${directory}/${file}-small.webp`);
-  });
