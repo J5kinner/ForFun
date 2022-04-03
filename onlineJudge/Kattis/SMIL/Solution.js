2decf7deead68d9867601f3a1bd2a4b912444a9e
Jonah Skinner <jonah.lee.skinner@gmail.com>
2022-04-03 19:21:58 +1000

    finding Smilies problem

--- onlineJudge/Kattis/SMIL/Solution.js
+++ onlineJudge/Kattis/SMIL/Solution.js
@@ -6,5 +6,29 @@ const rl = readline.createInterface({
 });
 
 rl.on("line", (line) => {
+    const str = line; 
+    let i = 0;
+    // console.log(str.split('').reverse());
+    const revStr = str.split('');
+    let ans = new Array();
+    revStr.forEach(x => {
+         if(x === ")"){
+             if(revStr[i-1] ===  "-"){
+                 if(revStr[i-2] === ":" || revStr[i-2] === ";"){
+                    console.log(i-2);
+
+                 }
+             }
+             if(revStr[i-1] === ":" || revStr[i-1] === ";" ){
+                console.log(i-1);
+
+             }
+        }
+
+        i++;
+        
+    });
+    // ans.forEach(i => console.log(i));
+    rl.close();
 
 })
