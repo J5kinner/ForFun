5d340254b465f3c41ac30eecefc8f56385b5aac2
Jonah Skinner <jonah.lee.skinner@gmail.com>
2022-04-06 16:13:49 +1000

    daily problems

---
+++ onlineJudge/Kattis/triangles/solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a * b * 0.5);
+
+    }
+}
