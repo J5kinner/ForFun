6145fd5633fec804bbccc87bdc927d5efea55fea
Jonah Skinner <jonah.lee.skinner@gmail.com>
2022-04-08 17:00:46 +1000

    Unfinished_Problem

--- onlineJudge/HackerRank/Citdadel/First_Unique_Character/solution.java
+++ onlineJudge/HackerRank/Citdadel/First_Unique_Character/solution.java
@@ -1,8 +1,25 @@
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class solution {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+
+        char arr[] = s.toCharArray();
+        HashMap<Character, Integer> unique = new HashMap<Character, Integer>();
+        Set<Character> nonUnique = new HashSet<>();
+        for(int i=0; i<s.length(); i++){
+            if(!unique.containsKey(arr[i])){
+                unique.put(arr[i], i);
+            }
         
+        }
+        for(int j=0; j<s.length(); j++){
+            if(unique.containsValue(j)){
+                System.out.println(j+1);
+            }
+        }
     }
 }
