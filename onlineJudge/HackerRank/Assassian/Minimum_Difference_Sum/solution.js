70abc20669b447083c01340425c0552f35babcd6
Jonah Skinner <jonah.lee.skinner@gmail.com>
2022-04-13 12:46:23 +1000

    minDiff done

---
+++ onlineJudge/HackerRank/Assassian/Minimum_Difference_Sum/solution.js
@@ -0,0 +1,11 @@
+const minDiff = (n, arr) => {
+    let totals = 0;
+    let sorted = arr.sort();
+    for(let i=0; i<n-1; i++){
+        totals += sorted[i+1] - sorted[i];        
+    }
+    return totals;
+}
+
+const total = minDiff(5, [1, 3, 3, 2, 4])
+console.log(total);
